a03b5a105a5805746066c96489c25f08de06d62f
michael-nwuju <michaelnwuju213@gmail.com>
2022-09-22 11:06:11 +0100

    fix: eslint-issues

--- src/Dashboard-Components/OTPay/OTPay.js
+++ src/Dashboard-Components/OTPay/OTPay.js
@@ -1,101 +1,87 @@
 import React, { useEffect, useState } from "react";
 import Button from "../../Components/Button/Button";
-import './OTPay.css'
+import "./OTPay.css";
 import { FormControl, FormErrorMessage, Image, Select } from "@chakra-ui/react";
-import {
-  Input,
-  InputGroup,
-  InputLeftElement,
-} from "@chakra-ui/react";
+import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
 import { useFormik } from "formik";
 import nairaIcon from "../../Assets/naira.png";
 import { useNavigate } from "react-router-dom";
 import { applicationState } from "../../data/state";
 import { useRecoilState } from "recoil";
 
-
-
 const defaultValues = { amount: "", plan: "" };
 
 const OTPay = () => {
-    const navigate = useNavigate();
-    const [application, setApplication] = useRecoilState(applicationState);
-    const [initialValues, setInitialValues] = useState(defaultValues);
-    const { values, errors, touched, handleChange, handleSubmit, setFieldValue } =
-    useFormik({
-      initialValues,
-      enableReinitialize: true,
-      onSubmit: values => {
-        setApplication({
-          ...application,
-          user: {
-            ...application.user,
-            other: {
-              amount: values.amount * 100, // convert to kobo
-              plan: values.plan,
-            },
+  const navigate = useNavigate();
+  const [application, setApplication] = useRecoilState(applicationState);
+  const [initialValues, setInitialValues] = useState(defaultValues);
+  const { values, errors, touched, handleChange, handleSubmit } = useFormik({
+    initialValues,
+    enableReinitialize: true,
+    onSubmit: values => {
+      setApplication({
+        ...application,
+        user: {
+          ...application.user,
+          other: {
+            amount: values.amount * 100, // convert to kobo
+            plan: values.plan,
           },
-        });
-        navigate("/verification");
-      },
-    });
+        },
+      });
+      navigate("/verification");
+    },
+  });
 
-    useEffect(() => {
-        setInitialValues({
-          amount: application.user.other.amount / 100, // convert to naira,
-          plan: application.user.other.plan,
-        });
-        // eslint-disable-next-line
-      }, []);
-    
+  useEffect(() => {
+    setInitialValues({
+      amount: application.user.other.amount / 100, // convert to naira,
+      plan: application.user.other.plan,
+    });
+    // eslint-disable-next-line
+  }, []);
 
   return (
-    <div className='otpay'>
-        <form onSubmit={handleSubmit} className="otp-form grid" >
+    <div className="otpay">
+      <form onSubmit={handleSubmit} className="otp-form grid">
+        <div className="otp-input grid">
+          <FormControl isInvalid={touched.amount && errors.amount}>
+            <label className="labels">Amount</label>
 
-            <div className="otp-input grid">
-              <FormControl
-                isInvalid={touched.amount && errors.amount}
-              >
-                <label className="labels">Amount</label>
+            <InputGroup>
+              <InputLeftElement
+                pointerEvents="none"
+                children={<Image w="40%" src={nairaIcon} />}
+              />
+              <Input
+                className="i-input"
+                type={"number"}
+                name="amount"
+                value={values.amount}
+                onChange={handleChange}
+                placeholder="Enter an amount"
+              />
+              <FormErrorMessage> {errors.amount} </FormErrorMessage>
+            </InputGroup>
+          </FormControl>
+        </div>
 
-                <InputGroup>
-                  <InputLeftElement
-                    pointerEvents="none"
-                    children={<Image w="40%" src={nairaIcon} />}
-                  />
-                  <Input
-                    className='i-input'
-                    type={"number"}
-                    name="amount"
-                    value={values.amount}
-                    onChange={handleChange}
-                    placeholder="Enter an amount"
-                  />
-                  <FormErrorMessage> {errors.amount} </FormErrorMessage>
-                </InputGroup>
-              </FormControl>
-            </div>
+        <div className="otp-input grid">
+          <label htmlFor="deduct" className="labels">
+            Deduct from
+          </label>
+          <Select className="s-input" disabled={""} placeholder="" id="deduct">
+            <option value=""></option>
+            <option value=""></option>
+          </Select>
+        </div>
 
-            <div className="otp-input grid">   
-                <label htmlFor='deduct' className="labels">Deduct from</label>
-                <Select
-                    className='s-input'
-                    disabled={''}
-                    placeholder=""
-                    id='deduct'
-                >
-                    <option value=""></option>
-                    <option value=""></option>
-                </Select>
-            </div>
-
-            <div className="dash-btn grid">
-              <Button text="PAY NOW" type={"submit"} />
-            </div>
-        </form>
+        <div className="dash-btn grid">
+          <Button text="PAY NOW" type={"submit"} />
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default OTPay
+export default OTPay;
--- src/Dashboard-Components/PlanCardAlt/PlanCardAlt.js
+++ src/Dashboard-Components/PlanCardAlt/PlanCardAlt.js
@@ -1,6 +1,6 @@
-import React from 'react'
-import './PlanCardAlt.css'
-import {CircularProgress} from '@chakra-ui/react'
+import React from "react";
+import "./PlanCardAlt.css";
+import { CircularProgress } from "@chakra-ui/react";
 import { useRecoilState } from "recoil";
 import { applicationState, singleProductState } from "../../data/state";
 import {
@@ -13,150 +13,140 @@ import {
 } from "@chakra-ui/react";
 import { Select } from "@chakra-ui/react";
 
-
-
-
-
 const PlanCardAlt = () => {
   const plans = [
     { name: "Pay Monthly", value: "recurring" },
     { name: "Pay Now", value: "once" },
   ];
 
+  // eslint-disable-next-line
   const [product] = useRecoilState(singleProductState);
   const [application, setApplication] = useRecoilState(applicationState);
   const oneTime = application.meta.plan === plans[1].value;
 
-
-
   const Data = [
     {
       plan: "Down payment",
-      color:'#E3000F',
+      color: "#E3000F",
     },
     {
       plan: "1st instalment",
-      color:'#8A8A8A',
+      color: "#8A8A8A",
     },
     {
       plan: "2nd instalment",
-      color:'#8A8A8A',
+      color: "#8A8A8A",
     },
     {
       plan: "3rd instalment",
-      color:'#8A8A8A',
+      color: "#8A8A8A",
     },
-];
+  ];
 
   return (
-    <div className='planCard'>
-
-      <div className='plan-conainer2 grid'>
-          
-        <div className='planCardAlt-left grid'>
-          <CircularProgress value={30} size='120px' color='#E3000F' thickness='16px' className='plan-progress' />
+    <div className="planCard">
+      <div className="plan-conainer2 grid">
+        <div className="planCardAlt-left grid">
+          <CircularProgress
+            value={30}
+            size="120px"
+            color="#E3000F"
+            thickness="16px"
+            className="plan-progress"
+          />
         </div>
-        <div className='planCardAlt-right grid'>
-
-              <div className='plan-input grid'>
+        <div className="planCardAlt-right grid">
+          <div className="plan-input grid">
+            <Select
+              className="n-input"
+              disabled={oneTime}
+              onChange={event =>
+                setApplication({
+                  ...application,
+                  meta: {
+                    ...application.meta,
+                    terms: {
+                      ...application.meta.terms,
+                      type: event.target.value,
+                    },
+                  },
+                })
+              }
+              placeholder="Monthly"
+              maxW="120px"
+            >
+              <option value="weekly">Weekly</option>
+              <option value="monthly">Monthly</option>
+            </Select>
+            <div>
+              <Flex>
+                <NumberInput
+                  isDisabled={oneTime}
+                  size="md"
+                  mr=""
+                  maxW="60px"
+                  max={6}
+                  defaultValue={application.meta.terms.tenure}
+                  onChange={value =>
+                    setApplication({
+                      ...application,
+                      meta: {
+                        ...application.meta,
+                        terms: {
+                          ...application.meta.terms,
+                          tenure: Number(value),
+                        },
+                      },
+                    })
+                  }
+                  min={1}
+                >
+                  <NumberInputField className="n-input" />
+                  <NumberInputStepper>
+                    <NumberIncrementStepper />
+                    <NumberDecrementStepper />
+                  </NumberInputStepper>
+                </NumberInput>
 
                 <Select
-                    className='n-input'
-                    disabled={oneTime}
-                    onChange={event =>
-                      setApplication({
-                        ...application,
-                        meta: {
-                          ...application.meta,
-                          terms: {
-                            ...application.meta.terms,
-                            type: event.target.value,
-                          },
+                  className="n-input"
+                  disabled={oneTime}
+                  onChange={event =>
+                    setApplication({
+                      ...application,
+                      meta: {
+                        ...application.meta,
+                        terms: {
+                          ...application.meta.terms,
+                          type: event.target.value,
                         },
-                      })
-                    }
-                    placeholder="Monthly"
-                    maxW="120px"
+                      },
+                    })
+                  }
+                  placeholder="Select"
+                  maxW="120px"
                 >
-                    <option value="weekly">Weekly</option>
-                    <option value="monthly">Monthly</option>
+                  <option value="weeks">Weeks</option>
+                  <option value="months">Months</option>
                 </Select>
-              <div>
-
-              <Flex>
-                  <NumberInput
-                   
-                    isDisabled={oneTime}
-                    size="md"
-                    mr=""
-                    maxW="60px"
-                    max={6}
-                    defaultValue={application.meta.terms.tenure}
-                    onChange={value =>
-                      setApplication({
-                        ...application,
-                        meta: {
-                          ...application.meta,
-                          terms: {
-                            ...application.meta.terms,
-                            tenure: Number(value),
-                          },
-                        },
-                      })
-                    }
-                    min={1}
-                  >
-                    <NumberInputField  className='n-input'/>
-                    <NumberInputStepper>
-                      <NumberIncrementStepper />
-                      <NumberDecrementStepper />
-                    </NumberInputStepper>
-                  </NumberInput>
-
-                  <Select
-                    className='n-input'
-                    disabled={oneTime}
-                    onChange={event =>
-                      setApplication({
-                        ...application,
-                        meta: {
-                          ...application.meta,
-                          terms: {
-                            ...application.meta.terms,
-                            type: event.target.value,
-                          },
-                        },
-                      })
-                    }
-                    placeholder="Select"
-                    maxW="120px"
-                  >
-                    <option value="weeks">Weeks</option>
-                    <option value="months">Months</option>
-                  </Select>
-
-                </Flex>
-              </div>
-               
+              </Flex>
+            </div>
           </div>
-          
-          <div className='planAlt-keys grid'>
 
+          <div className="planAlt-keys grid">
             {Data.map((item, index) => {
-            return (
-              <div key={index} className='plan-key flex'>
-                  <span style={{background:item.color}}></span> <p>{item.plan}</p>
-              </div>
-            );
+              return (
+                <div key={index} className="plan-key flex">
+                  <span style={{ background: item.color }}></span>{" "}
+                  <p>{item.plan}</p>
+                </div>
+              );
             })}
-
           </div>
-          
         </div>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PlanCardAlt
+export default PlanCardAlt;
