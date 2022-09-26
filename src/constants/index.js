242426b4c960fec9302f5c54093590e560d38ba1
michael-nwuju <michaelnwuju213@gmail.com>
2022-09-26 11:07:35 +0100

    feat: redeploy code for api

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,5 @@
 export const dev = process.env.NODE_ENV === "development";
 
-export const API_URI = dev
+export const API_URI = !dev
   ? process.env.REACT_APP_STAGING_API_URI
   : process.env.REACT_APP_LIVE_API_URI;
