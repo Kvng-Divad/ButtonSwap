c153049e986938db3a8b5e85fcd45b3cfd7e1d3e
michael-nwuju <michaelnwuju213@gmail.com>
2022-09-26 09:40:27 +0100

    Remove goodluck's push

---
+++ src/Routes/Signin.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Application-Component/Header/Header";
+import SignIn from "../Application-Component/Auth/SignIn";
+
+const Signin = () => {
+  return (
+    <div className="Signin">
+      <Header />
+      <SignIn />
+    </div>
+  );
+};
+
+export default Signin;
