0431218f544c06a323d798739b39cd5ab4ef788e
michael-nwuju <michaelnwuju213@gmail.com>
2022-09-28 11:06:30 +0100

    feat: fix merge conflict issues

--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -1,41 +1,35 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import styled from 'styled-components'
+import styled from "styled-components";
 
-
-const Button = ({text}) => {
+const Button = ({ text }) => {
   return (
     <Btn>
-      <Link to='/products'>
-      <button className=''>
-            {text}
-        </button>
+      <Link to="/products">
+        <button className="">{text}</button>
       </Link>
-        
     </Btn>
-   
-  )
-}
+  );
+};
 
 export default Button;
 
-const Btn = styled.button`
-    background:transparent;
-    button{
-      background: var(--skin-color);
-      border: 1px solid var(--skin-color);
-      color: var(--text-color-alt);
-      font-family: var(--bdy-font-family);
-      font-size: var(--font-size-sm);
-      font-weight: 700;
-      padding: .5rem;
-      text-transform: uppercase;
-   }
-   button:hover{
-      background: transparent;
-      border: 1px solid var(--skin-color);
-      color: var(--text-color);
-   }
-   
+export const Btn = styled.button`
+  background: transparent;
+  button {
+    background: var(--skin-color);
+    border: 1px solid var(--skin-color);
+    color: var(--text-color-alt);
+    font-family: var(--bdy-font-family);
+    font-size: var(--font-size-sm);
+    font-weight: 700;
+    padding: 0.5rem;
+    text-transform: uppercase;
+  }
+  button:hover {
+    background: transparent;
+    border: 1px solid var(--skin-color);
+    color: var(--text-color);
+  }
 `;
